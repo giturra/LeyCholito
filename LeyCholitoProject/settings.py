d6cb4982cad5cb2e6fc3f2925ca127de6c343558
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2018-02-26 11:56:02 -0300

    registers models on interface administration

--- LeyCholitoProject/settings.py
+++ LeyCholitoProject/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'naturalUser',
     'animal',
-    'complaint'
+    'complaint',
+    'ONG',
+    'municipality'
 ]
 
 MIDDLEWARE = [
--- ONG/admin.py
+++ ONG/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ong
 
-# Register your models here.
+
+admin.site.register(Ong)
